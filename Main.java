f312f4929826cfdb27744cfabe4d970c8afde484
naveen84 <naveenachari39@gmail.com>
2018-09-10 13:11:07 +0530

    This is a remote branch option commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ System.out.println("Hello World");
 int x=0;
 System.out.println("This line added from GitHub.com");
 System.out.println("This line added to check Branch option in Git");
+System.out.println("This line added to check Remote Branch option");
 }
 }
