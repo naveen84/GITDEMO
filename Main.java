26aff7f1d4d2d4f84224ccc699082977f607e1d6
naveen84 <naveenachari39@gmail.com>
2018-09-10 13:01:02 +0530

    This third commit for Branch

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public static void main(String[]a)
 System.out.println("Hello World");
 int x=0;
 System.out.println("This line added from GitHub.com");
+System.out.println("This line added to check Branch option in Git");
 }
 }
